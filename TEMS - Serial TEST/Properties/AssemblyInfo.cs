f112fc7b6b3988c45923459da020281871bab46b
Lobet Mathieu <omlet05@gmail.com>
2015-01-04 01:27:44 +0100

    project renaming
    TEMS - Manager is the final name for the C# Manager App.

--- TEMS - Serial TEST/Properties/AssemblyInfo.cs
+++ TEMS - Serial TEST/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("TEMS - Serial TEST")]
+[assembly: AssemblyTitle("TEMS - Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TEMS - Serial TEST")]
+[assembly: AssemblyProduct("TEMS - Manager")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
